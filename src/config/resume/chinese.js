3cf8b97e75f3dd70930f03543449ce7378795900
罗晓彬 <641361968@qq.com>
2023-06-03 13:28:53 +0800

    feat: initial

---
+++ src/config/resume/chinese.js
@@ -0,0 +1,34 @@
+const text = `
+雷淑兰
+---
+web前端开发
+
+基本信息
+---
+-   姓名 雷淑兰
+-   学历 本科
+-   毕业院校 哈尔滨商业大学
+-   年龄 28岁
+
+技能点
+---
+-   Javascript
+-   English (CET-6)
+
+技术栈
+---
+-   前端 Vue.js, Mini-Program, Hybrid
+
+教育经历
+---
+-  哈尔滨商业大学 - 烹饪与营养教育 (2012-2016)
+
+联系方式
+---
+-   手机 18245108572
+-   [邮箱](754297923@qq.com)
+-   [Github](https://github.com/freya1106)
+
+`
+
+export default text
